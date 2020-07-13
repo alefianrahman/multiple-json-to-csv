ab7a6d45a50b0b6e73097955bf94b42bb7467156
Alfian Rahman <alfian.rahman@transdigital.co.id>
2020-07-13 18:45:54 +0700

    Create a class.

--- multiple_json_to_csv.py
+++ multiple_json_to_csv.py
@@ -1,45 +1,36 @@
-import pandas as pd
-import numpy as np
 import os
-
-"""
-This library will receive multiple JSON files in a folder, combine the 
-files, and output the files to a CSV file.
-"""
-
-json_path = '/path/to/json'  # Path to JSON files
-output_name = 'results.csv'  # Output filename
+import pandas as pd
+import sys
 
 
-def multiple_json_to_csv(path=json_path, filename=output_name):
-    """
-    This function will receive multiple JSON files in a folder, combine the
-    files, and output the files to a CSV file.
-    """
-    list_data = []  # initiate empty list
+class MultipleJSONtoCSV:
+	"""An object that can convert multiple JSON files to a CSV file."""
+	def __init__(self, path, output):
+		"""
+		Initialize object's attributes.
 
-    # Combine multiple JSON files into a list of Data Frames
-    for file in (os.listdir(path)):
-        if '.json' in file:
-            file_path = json_path + os.sep + file
-            list_data.append(pd.read_json(file_path))
+		:param str path: Path to JSON files.
+		:param str output: Output file name.
+		"""
+		self.path = path
+		self.output = output
 
-    all_data = None  # Initiate a dummy variable
-
-    # Merge all Data Frames
-    for data in list_data:
-        if all_data is None:
-            all_data = data
-        else:
-            all_data.append(data)
-
-    # Convert the Data Frame to a CSV file
-    all_data.to_csv(filename, index=False)
+	def convert_json_to_csv(self):
+		"""Convert multiple JSON files to a CSV file."""
+		list_df = []
+		for file in os.listdir(self.path):
+			if file.endswith('.json'):
+				file_path = self.path + file
+				df = pd.read_json(file_path)
+				list_df.append(df)
+		all_df = pd.concat(list_df)
+		all_df.to_csv(self.output, index=False)
+		print("All JSON files in {} are successfully converted into a CSV "
+			  "file named {}".format(self.path, self.output))
 
 
 if __name__ == '__main__':
-    multiple_json_to_csv()
-
-
-
-
+	json_path = sys.argv[1]  # Path to JSON files
+	output_name = sys.argv[2]  # Output filename
+	obj = MultipleJSONtoCSV(json_path, output_name)
+	obj.convert_json_to_csv()
