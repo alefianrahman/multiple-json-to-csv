bf148554f5e06fd9d18d644c9088ee41e9ac94f5
Alfian Rahman <alfian.rahman@transdigital.co.id>
2020-07-20 17:59:29 +0700

    Refactor multiple_json_to_csv.py

--- multiple_json_to_csv.py
+++ multiple_json_to_csv.py
@@ -3,34 +3,34 @@ import pandas as pd
 import sys
 
 
-class MultipleJSONtoCSV:
+class MultipleJSONToCSV:
 	"""An object that can convert multiple JSON files to a CSV file."""
-	def __init__(self, path, output):
+	def __init__(self, source_path, destination_path):
 		"""
 		Initialize object's attributes.
 
 		:param str path: Path to JSON files.
 		:param str output: Output file name.
 		"""
-		self.path = path
-		self.output = output
+		self.source_path = source_path
+		self.destination_path = destination_path
 
 	def convert_json_to_csv(self):
 		"""Convert multiple JSON files to a CSV file."""
 		list_df = []
-		for file in os.listdir(self.path):
+		for file in os.listdir(self.source_path):
 			if file.endswith('.json'):
-				file_path = self.path + file
-				df = pd.read_json(file_path)
+				source_file_path = self.source_path + file
+				df = pd.read_json(source_file_path)
 				list_df.append(df)
 		all_df = pd.concat(list_df)
-		all_df.to_csv(self.output, index=False)
+		all_df.to_csv(self.destination_path, index=False)
 		print("All JSON files in {} are successfully converted into a CSV "
-			  "file named {}".format(self.path, self.output))
+			  "file named {}".format(self.source_path, self.destination_path))
 
 
 if __name__ == '__main__':
 	json_path = sys.argv[1]  # Path to JSON files
-	output_name = sys.argv[2]  # Output filename
-	obj = MultipleJSONtoCSV(json_path, output_name)
-	obj.convert_json_to_csv()
+	output_path = sys.argv[2]  # Output path
+	converter = MultipleJSONToCSV(json_path, output_path)
+	converter.convert_json_to_csv()
